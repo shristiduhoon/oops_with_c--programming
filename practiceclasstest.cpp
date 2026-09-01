470cbd8ecaf1d9c62db987874741f7434d0f0f8b
ShristiDuhhon <shristiduhoon24@gmail.com>
2026-09-01 16:14:13 +0530

    test

--- practiceclasstest.cpp
+++ practiceclasstest.cpp
@@ -1,32 +1,83 @@
+// #include <iostream>
+// using namespace std;
+// class employee{
+//     private:
+//     int id,salary;
+//     string name;
+//     public:
+//     void input();
+//     void display();
+    
+// };
+// void employee::input(){
+//         cout<<"Enter id";
+//         cin>>id;
+//         cout<<"enter name";
+//         cin>>name;
+//         cout<<"enter salary:";
+//         cin>>salary;
+// }
+// void employee::display(){
+//         cout<<"id:"<<id<<endl;
+//         cout<<" name:"<<name<<endl;
+//         cout<<"salary:"<<salary<<endl;
+// }
+
+
+// int main(){
+//     employee s;
+//     s.input();
+//     s.display();
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// class student{
+//     public:
+//     int roll,marks;
+//     string name;
+ 
+//     void input(){
+//         cout<<"Enter roll no.";
+//         cin>>roll;
+//         cout<<"enter name:";
+//         cin>>name;
+//         cout<<"enter marks:";
+//         cin>>marks;
+//     }
+//     void display(){
+//         cout<<"roll:"<<roll<<endl;
+//         cout<<" name:"<<name<<endl;
+//         cout<<"marks:"<<marks<<endl;
+// }
+// };
+// int main(){
+//     student s;
+//     s.input();
+//     s.display();
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-class employee{
-    private:
-    int id,salary;
-    string name;
-    public:
-    void input();
-    void display();
-    
-};
-void employee::input(){
-        cout<<"Enter id";
-        cin>>id;
-        cout<<"enter name";
-        cin>>name;
-        cout<<"enter salary:";
-        cin>>salary;
+void valueswap(int a,int b){
+    int temp=a;
+    a=b;
+    b=temp;
 }
-void employee::display(){
-        cout<<"id:"<<id<<endl;
-        cout<<" name:"<<name<<endl;
-        cout<<"salary:"<<salary<<endl;
+void refswap(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
 }
-
-
 int main(){
-    employee s;
-    s.input();
-    s.display();
-    return 0;
+    int x=10;
+    int y=20;
+    cout<<"before swap "<<"x="<<x<<" y="<<y<<endl;
+    valueswap(x,y);
+    cout<<"After call by value swap "<<"x="<<x<<" y="<<y<<endl;
+    refswap(x,y);
+    cout<<"after call by refrence swap "<<"x="<<x<<" y="<<y<<endl;
 }
